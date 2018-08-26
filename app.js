b91de1fb6d68254e69af93411a756d74bc2ea45a
j-hernandezinzunza <itsjosehernandez@gmail.com>
2018-08-26 17:46:13 -0600

    JavaScript modules 83-85

--- app.js
+++ app.js
@@ -1,63 +1,27 @@
-const takeOrder = () => {
-    console.log("Order: pizza");
-};
-
-takeOrder();
-
-const takeOrder = (topping) => {
-    console.log('Order: pizza topped with ' + topping);
-};
-
-takeOrder("mush");
-
-const takeOrder = (topping, crustType) => {
-    console.log("Order: " + crustType + " crust pizza topped with " + topping);
-};
-
-takeOrder('mushrooms', 'thin');
-takeOrder('mushrooms', 'deep');
-takeOrder('mushrooms', 'hand tossed');
-
-let orderCount = 0;
-
-const takeOrder = (topping, crustType) => {
-    orderCount++;
-    console.log('Order: ' + crustType + ' pizza topped with ' + topping);
-};
-
-const getSubTotal = (itemCount) => {
-    return itemCount * 7.5;
-};
-
-takeOrder('mushroom', 'thin crust');
-takeOrder('spinach', 'whole wheat');
-takeOrder('pepperoni', 'brooklyn style');
-console.log(getSubTotal(orderCount));
-
-
-let orderCount = 0;
-
-const takeOrder = (topping, crustType) => {
-    orderCount++;
-    console.log('Order: ' + crustType + ' pizza topped with ' + topping);
-};
-
-takeOrder('mushroom', 'thin crust');
-takeOrder('spinach', 'whole wheat');
-takeOrder('pepperoni', 'brooklyn style');
-
-const getSubTotal = (itemCount) => {
-    return itemCount * 7.5;
-};
-
-const getTax = (itemCount) => {
-    return getSubTotal(itemCount) * 0.06
+function isGreaterThan(numberOne, numberTwo) {
+    if (numberOne > numberTwo) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-const getTotal = (itemCount) => {
-    return getTax(itemCount) + getSubTotal(itemCount)
+isGreaterThan(3, 4);
+
+
+
+const isGreaterThan = (numberOne, numberTwo) => {
+    if (numberOne > numberTwo) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-console.log(getSubTotal(orderCount));
+isGreaterThan(4, 8)
 
-console.log(getTotal(orderCount))
+const volumeOfSphere = diameter => (1 / 6) * Math.PI * diameter * diameter * diameter;
+
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
+
+
