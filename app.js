41dedf557bf069c437d5c2b1155e5d3f409a88eb
j-hernandezinzunza <itsjosehernandez@gmail.com>
2018-08-26 15:26:16 -0600

    JavaScript Modules 10-18

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
-console.log("Hello!");
-console.log("Pizza Rules!");
+// This line of code sets the variable location to the string New York City
+const location = 'New York City';
 
-console.log("JavaScript");
-console.log(33.7);
-console.log(true);
-console.log(null);
+// This line of code sets the variable latitude to the number 40.7
+let latitude = 40.7;
 
-console.log(29 + 3.5);
-console.log(2018 - 1969);
-console.log(65 / 240);
-console.log(0.2708 * 100);
+// This line of code sets the variable inNorthernHemisphere to true
+let inNorthernHemisphere = true;
 
-console.log("Teaching the world how to code".length);
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
 
-// Log Codecademy in all uppercase letters
-console.log('Codecademy'.toUpperCase());
+const entree = 'Enchiladas';
+const price = 12;
+console.log(entree);
+console.log(price);
 
-// Use a string method to log the following statment without whitespace at the beginning and end of it.
-console.log('    Remove whitespace   '.trim()); 
+entree = 'Tacos';
 
-console.log(Math.floor(Math.random() * 100));
-console.log(Math.ceil(43.8));
-console.log(Number.isInteger(2017));
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+
+let notDefined;
+console.log(notDefined);
+
+let valueless;
+console.log(valueless);
+
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+
+// Add and assign to molecule below
+molecule += 16;
+
+// Multiply and assign to particle below
+particle *= 6.02;
+
+// Increment assay by 1
+assay++;
+
+let favoriteAnimal = 'Turtle';
+console.log("My favorite animal: " + favoriteAnimal);
+
+let myName = "Jose";
+let myCity = "Aurora";
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
 
 
-// Opening Line.
-console.log('It was love at first sight.');
-/*
-console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
-console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
-console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
-console.log('If it became jaundice they could treat it.');
-console.log('If it didn\'t become jaundice and went away they could discharge him.');
-console.log('But this just being short of jaundice all the time confused them.');
-*/
