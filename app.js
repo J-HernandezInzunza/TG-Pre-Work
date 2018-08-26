18252a7633e4c891cb462a5b500fea5083c73935
j-hernandezinzunza <itsjosehernandez@gmail.com>
2018-08-26 15:33:02 -0600

    JavaScript Modules 19-32

--- app.js
+++ app.js
@@ -1,51 +1,10 @@
-// This line of code sets the variable location to the string New York City
-const location = 'New York City';
+//initialize kelvin variable
+let kelvin = 293;
+//celcius is 273 less than kelvin
+let celsius = kelvin - 273;
+//use equation to determine f
+let fahrenheit = ((celsius * (9 / 5)) + 32);
+//round down to nearest integer
+fahrenheit = Math.floor(fahrenheit);
 
-// This line of code sets the variable latitude to the number 40.7
-let latitude = 40.7;
-
-// This line of code sets the variable inNorthernHemisphere to true
-let inNorthernHemisphere = true;
-
-console.log(location);
-console.log(latitude);
-console.log(inNorthernHemisphere);
-
-const entree = 'Enchiladas';
-const price = 12;
-console.log(entree);
-console.log(price);
-
-entree = 'Tacos';
-
-let changeMe = true;
-changeMe = false;
-console.log(changeMe);
-
-let notDefined;
-console.log(notDefined);
-
-let valueless;
-console.log(valueless);
-
-let molecule = 16;
-let particle = 18;
-let assay = 3;
-
-// Add and assign to molecule below
-molecule += 16;
-
-// Multiply and assign to particle below
-particle *= 6.02;
-
-// Increment assay by 1
-assay++;
-
-let favoriteAnimal = 'Turtle';
-console.log("My favorite animal: " + favoriteAnimal);
-
-let myName = "Jose";
-let myCity = "Aurora";
-console.log(`My name is ${myName}. My favorite city is ${myCity}`);
-
-
+console.log("The temperature is " + fahrenheit + " degrees Fahrenheit.");
