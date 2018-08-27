d06d0121f6931931e905b4201f5ecf77c2d28856
j-hernandezinzunza <itsjosehernandez@gmail.com>
2018-08-26 18:11:13 -0600

    JavaScript modules 86-100

--- app.js
+++ app.js
@@ -1,27 +1,70 @@
-function isGreaterThan(numberOne, numberTwo) {
-    if (numberOne > numberTwo) {
-        return true;
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' ||
+        userInput === 'paper' ||
+        userInput === 'scissors') {
+        //console.log(userInput);
+        return userInput;
     } else {
-        return false;
+        console.log("You Dun GOOFED!");
+    }
+};
+//getUserChoice('balls');
+
+const getComputerChoice = () => {
+    let choice = Math.floor((Math.random() * 2) + 1)
+
+    if (choice === 0) {
+        return 'rock';
+    } else if (choice === 1) {
+        return 'paper';
+    } else if (choice === 2) {
+        return 'scissors';
     }
 }
 
-isGreaterThan(3, 4);
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+        return "The game was a tie.";
+    }
 
-
-
-const isGreaterThan = (numberOne, numberTwo) => {
-    if (numberOne > numberTwo) {
-        return true;
-    } else {
-        return false;
+    if (userChoice = 'rock') {
+        if (computerChoice === 'paper') {
+            return "The computer wins this round!";
+        } else if (computerChoice === 'scissors') {
+            return "You won this round!";
+        }
+    }
+    if (userChoice = 'paper') {
+        if (computerChoice === 'scissors') {
+            return "The computer wins this round!";
+        } else if (computerChoice === 'rock') {
+            return "You won this round!";
+        }
+    }
+    if (userChoice = 'scissors') {
+        if (computerChoice === 'rock') {
+            return "The computer wins this round!";
+        } else if (computerChoice === 'paper') {
+            return "You won this round!";
+        }
     }
 }
 
-isGreaterThan(4, 8)
+const playGame = () => {
+    let userChoice = getUserChoice('rock');
+    console.log(userChoice);
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    //determine winner
+    return determineWinner(userChoice, computerChoice);
+};
 
-const volumeOfSphere = diameter => (1 / 6) * Math.PI * diameter * diameter * diameter;
+console.log(playGame());
+//console.log(determineWinner( getUserChoice('rock') , getComputerChoice() ) );
 
-console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
-
-
+/*
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+console.log (getComputerChoice());
+*/
