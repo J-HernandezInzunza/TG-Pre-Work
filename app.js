b1bc58ffd7863d78c4481ebaf855ad0540e0069c
j-hernandezinzunza <itsjosehernandez@gmail.com>
2018-08-27 16:04:57 -0600

    JavaScript modules 281-290 (Requests)

--- app.js
+++ app.js
@@ -1,70 +1,139 @@
-//messagemixer.js info
-const MessageMixer = {};
+console.log('First message!');
+setTimeout(() => {
+    console.log('This message will always run last...');
+}, 0);
+console.log('Second message!');
 
-MessageMixer.countCharacter = function (inputString, inputCharacter) {
-    let count = 0;
-    let string = inputString.toLowerCase();
-    let character = inputCharacter.toLowerCase();
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] === character) {
-            count++;
+
+const xhr = new XMLHttpRequest();
+const url = "https://api-to-call.com/endpoint";
+
+xhr.responseType = 'json';
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        return xhr.response;
+    }
+};
+
+xhr.open('GET', url);
+xhr.send();
+
+// Information to reach API
+const url = "https://api.datamuse.com/words?";
+const queryParams = 'rel_rhy=';
+
+
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+    const wordQuery = inputField.value;
+    const endpoint = '' + url + queryParams + wordQuery;
+
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response);
         }
     }
-    return count;
-};
+    xhr.open('GET', endpoint);
+    xhr.send();
+}
 
-MessageMixer.capitalizeFirstCharacterOfWords = function (string) {
-    let arr = string.split(" ");
-    for (let i = 0; i < arr.length; i++) {
-        let word = arr[i];
-        arr[i] = word[0].toUpperCase() + word.substring(1);
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild);
+    };
+    getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
+
+
+// Information to reach API
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jjb=';
+const additionalParams = '&topics=';
+
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const topicField = document.querySelector('#topic');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+    const wordQuery = inputField.value;
+    const topicQuery = topicField.value;
+    const endpoint = `${url}${queryParams}${wordQuery}${additionalParams}${topicQuery}`;
+
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response);
+        }
     }
-    return arr.join(" ");
-};
 
-MessageMixer.reverseWord = function (word) {
-    return word.split("").reverse().join("");
-};
+    xhr.open('GET', endpoint);
+    xhr.send();
+}
 
-MessageMixer.reverseAllWords = function (sentence) {
-    let words = sentence.split(" ");
-    for (let i = 0; i < words.length; i++) {
-        words[i] = MessageMixer.reverseWord(words[i]);
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild);
     }
-    return words.join(" ");
-};
+    getSuggestions();
+}
 
-MessageMixer.replaceFirstOccurence = function (string, toBeReplaced, replaceWith) {
-    return string.replace(toBeReplaced, replaceWith);
-};
+submit.addEventListener('click', displaySuggestions);
 
-MessageMixer.replaceAllOccurrences = function (string, toBeReplaced, replaceWith) {
-    return string.split(toBeReplaced).join(replaceWith);
-};
 
-MessageMixer.encode = function (string) {
-    let replacementObject = { "a": "@", "s": "$", "i": "!", "o": "0" };
-    for (let key in replacementObject) {
-        string = MessageMixer.replaceAllOccurrences(string, key, replacementObject[key]);
+// Information to reach API
+const apiKey = '<Your API Key>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten });
+
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response);
+        }
     }
-    return string;
-};
-
-module.exports = MessageMixer;
-
-
-
-//message.js info
-const MessageMixer = require('./messageMixer.js');
-
-function displayMessage() {
-    console.log(MessageMixer.countCharacter("What is the color of the sky?", "t"));
-    console.log(MessageMixer.capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-    console.log(MessageMixer.reverseWord("What is the color of the sky?"));
-    console.log(MessageMixer.reverseAllWords("What is the color of the sky?"));
-    console.log(MessageMixer.replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-    console.log(MessageMixer.encode("What is the color of the sky?"));
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.setRequestHeader('apikey', apiKey);
+    xhr.send(data);
 }
 
 
-displayMessage();
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild);
+    }
+    shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
