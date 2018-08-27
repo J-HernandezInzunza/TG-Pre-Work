25996b028e02af95870df216d88c8f2becc04571
j-hernandezinzunza <itsjosehernandez@gmail.com>
2018-08-27 16:30:33 -0600

    JavaScript modules 295-300

--- app.js
+++ app.js
@@ -1,156 +1,3 @@
-console.log('First message!');
-setTimeout(() => {
-    console.log('This message will always run last...');
-}, 0);
-console.log('Second message!');
-
-
-const xhr = new XMLHttpRequest();
-const url = "https://api-to-call.com/endpoint";
-
-xhr.responseType = 'json';
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        return xhr.response;
-    }
-};
-
-xhr.open('GET', url);
-xhr.send();
-
-// Information to reach API
-const url = "https://api.datamuse.com/words?";
-const queryParams = 'rel_rhy=';
-
-
-// Selecting page elements
-const inputField = document.querySelector('#input');
-const submit = document.querySelector('#submit');
-const responseField = document.querySelector('#responseField');
-
-// AJAX function
-const getSuggestions = () => {
-    const wordQuery = inputField.value;
-    const endpoint = '' + url + queryParams + wordQuery;
-
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            renderResponse(xhr.response);
-        }
-    }
-    xhr.open('GET', endpoint);
-    xhr.send();
-}
-
-// Clear previous results and display results to webpage
-const displaySuggestions = (event) => {
-    event.preventDefault();
-    while (responseField.firstChild) {
-        responseField.removeChild(responseField.firstChild);
-    };
-    getSuggestions();
-}
-
-submit.addEventListener('click', displaySuggestions);
-
-
-// Information to reach API
-const url = 'https://api.datamuse.com/words?';
-const queryParams = 'rel_jjb=';
-const additionalParams = '&topics=';
-
-// Selecting page elements
-const inputField = document.querySelector('#input');
-const topicField = document.querySelector('#topic');
-const submit = document.querySelector('#submit');
-const responseField = document.querySelector('#responseField');
-
-// AJAX function
-const getSuggestions = () => {
-    const wordQuery = inputField.value;
-    const topicQuery = topicField.value;
-    const endpoint = `${url}${queryParams}${wordQuery}${additionalParams}${topicQuery}`;
-
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            renderResponse(xhr.response);
-        }
-    }
-
-    xhr.open('GET', endpoint);
-    xhr.send();
-}
-
-// Clear previous results and display results to webpage
-const displaySuggestions = (event) => {
-    event.preventDefault();
-    while (responseField.firstChild) {
-        responseField.removeChild(responseField.firstChild);
-    }
-    getSuggestions();
-}
-
-submit.addEventListener('click', displaySuggestions);
-
-
-// Information to reach API
-const apiKey = '<Your API Key>';
-const url = 'https://api.rebrandly.com/v1/links';
-
-// Some page elements
-const inputField = document.querySelector('#input');
-const shortenButton = document.querySelector('#shorten');
-const responseField = document.querySelector('#responseField');
-
-// AJAX functions
-const shortenUrl = () => {
-    const urlToShorten = inputField.value;
-    const data = JSON.stringify({ destination: urlToShorten });
-
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            renderResponse(xhr.response);
-        }
-    }
-    xhr.open('POST', url);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.setRequestHeader('apikey', apiKey);
-    xhr.send(data);
-}
-
-
-// Clear page and call AJAX functions
-const displayShortUrl = (event) => {
-    event.preventDefault();
-    while (responseField.firstChild) {
-        responseField.removeChild(responseField.firstChild);
-    }
-    shortenUrl();
-}
-
-shortenButton.addEventListener('click', displayShortUrl);
-
-
-fetch('https://api-to-call.com/endpoint').then(response => {
-    if (response.ok) {
-        return response.json();
-    }
-    throw new Error('Request failed!');
-}, networkError => {
-    console.log(networkError.message);
-}).then(jsonResponse => {
-    return jsonResponse;
-})
-
-
 // Information to reach API
 const url = 'https://api.datamuse.com/words';
 const queryParams = '?sl=';
@@ -172,6 +19,8 @@ const getSuggestions = () => {
         throw new Error('Request failed!');
     }, networkError => {
         console.log(networkError.message)
+    }).then(jsonResponse => {
+        renderResponse(jsonResponse);
     })
 }
 
@@ -185,3 +34,140 @@ const displaySuggestions = (event) => {
 };
 
 submit.addEventListener('click', displaySuggestions);
+
+fetch('https://api-to-call.com/endpoint', {
+    method: 'POST',
+    body: JSON.stringify({ id: "200" })
+}).then(response => {
+    if (response.ok) {
+        return response.json();
+    }
+    throw new Error('Request failed!');
+}, networkError => {
+    console.log(networkError.message);
+}).then(jsonResponse => {
+    console.log(jsonResponse);
+    })
+
+// Information to reach API
+const apiKey = '<Your API Key>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten })
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'apikey': apiKey
+        },
+        body: data
+    })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild)
+    }
+    shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+
+// Information to reach API
+const apiKey = '<Your API Key>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten })
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'apikey': apiKey
+        },
+        body: data
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed!');
+    }, networkError => {
+        console.log(networkError.message)
+    })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild)
+    }
+    shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+
+// Information to reach API
+const apiKey = '<Your API Key>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten })
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'apikey': apiKey
+        },
+        body: data
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed!');
+    }, networkError => {
+        console.log(networkError.message)
+    }).then(jsonResponse => {
+        renderResponse(jsonResponse);
+    })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild)
+    }
+    shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
