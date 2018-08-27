031d944bc6dd68e03d4d37890808cd2d7577853e
j-hernandezinzunza <itsjosehernandez@gmail.com>
2018-08-26 22:47:54 -0600

    JavaScript modules 184-192 (Mini Linter Project)

--- app.js
+++ app.js
@@ -1,88 +1,44 @@
-let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
 
-// Iterate over fruits below
-fruits.forEach(function (fruitItem) {
-    console.log(' - ' + fruitItem);
+let overusedWords = ['really', 'very', 'basically'];
+
+let unnecessaryWords = ['extremely', 'literally', 'actually'];
+
+
+let storyWords = story.split(' ');
+console.log(storyWords.length);
+//loop and compare arrays to remove uneccessary words
+let betterWords = storyWords.filter(function (word) {
+    if (unnecessaryWords.includes(word) === false) {
+        //add to new array
+        return word;
+    } else {
+        //Do nothing with this word
+    }
 });
-
-fruits.forEach(fruitItem => console.log(fruitItem));
-
-
-let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
-
-// Create the secretMessage array below
-/*
-let secretMessage = animals.map(function(animal){
-  return animal[0];
+//console.log(betterWords);
+let reallyCounter = 0;
+let veryCounter = 0;
+let basicallyCounter = 0;
+let overused = betterWords.forEach(function (word) {
+    if (overusedWords[0] === word) {
+        reallyCounter++;
+    } else if (overusedWords[1] === word) {
+        veryCounter++;
+    } else if (overusedWords[2] === word) {
+        basicallyCounter++;
+    }
 });
-console.log(secretMessage.join(''));
-*/
+console.log('You used the word "really" ' + reallyCounter + ' times.');
+console.log('You used the word "very" ' + veryCounter + ' times.');
+console.log('You used the word "basically" ' + basicallyCounter + ' times.');
 
-let secretMessage = animals.map(animal => animal[0]);
-console.log(secretMessage.join(''));
-
-let bigNumbers = [100, 200, 300, 400, 500];
-
-// Create the smallNumbers array below
-let smallNumbers = bigNumbers.map(function (numbers) {
-    return numbers / 100;
+//count sentences
+let sentences = 0;
+betterWords.forEach(word => {
+    if (word[word.length - 1] === '.' || word[word.length - 1] === '!') {
+        sentences += 1;
+    }
 });
-console.log(smallNumbers);
-/*
-let smallNumbers2 = bigNumbers.map(numbers => numbers / 100);
-console.log(smallNumbers2);
-*/
-
-let randomNumbers = [375, 200, 3.14, 7, 13, 852];
-
-// Call .filter() on randomNumbers below
-let smallNumbers = randomNumbers.filter(function (number) {
-    return number < 250;
-});
-
-let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
-
-/*
-let longFavoriteWords = favoriteWords.filter(function(word) {
-  return word.length > 7;
-});
-*/
-
-// Refactor the code above using arrow function syntax
-let longFavoriteWords = favoriteWords.filter(word => word.length > 7);
-
-let randomNumbers = [375, 200, 3.14, 7, 13, 852];
-
-// Call .filter() on randomNumbers below
-let smallNumbers = randomNumbers.filter(function (number) {
-    return number < 250;
-});
-
-let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
-
-/*
-let longFavoriteWords = favoriteWords.filter(function(word) {
-  return word.length > 7;
-});
-*/
-
-// Refactor the code above using arrow function syntax
-let longFavoriteWords = favoriteWords.filter(word => word.length > 7);
-
-let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
-
-let nums = [1, 50, 75, 200, 350, 525, 1000];
-
-//  Choose a method that will return undefined
-cities.forEach(city => console.log('Have you visited ' + city + '?'));
-
-// Choose a method that will return a new array
-let longCities = cities.filter(city => city.length > 7);
-
-// Choose a method that will return a new array
-let smallerNums = nums.map(num => num - 5);
-
-// Choose a method that will return a boolean value
-nums.some(num => num < 0);
-
-
+console.log(sentences);
+console.log(betterWords.join(' '));
