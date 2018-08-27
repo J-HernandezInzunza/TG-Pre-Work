c128737d5df678ff26596624d3e90295878d360c
j-hernandezinzunza <itsjosehernandez@gmail.com>
2018-08-27 12:22:23 -0600

    JavaScript modules 223-248

--- app.js
+++ app.js
@@ -1,237 +1,56 @@
-class Surgeon {
-    constructor(name, department) {
-        this.name = name;
-        this.deparment = department;
+class Media {
+    constructor(title, ratings, isCheckedOut) {
+        this._title = title;
+        this._rating = ratings;
+        this._isCheckedOut = isCheckedOut;
+    }
+    //Basic getter and setters
+    get rating() {
+        return this._rating;
+    }
+    get title() {
+        return this._title;
+    }
+    get isCheckedOut() {
+        return this._IsCheckedOut;
+    }
+    //Other methods
+    getAverageRating() {
+        let ratingsSum = 0;
+        for (let i = 0; i < this._rating.length; i++) {
+            ratingsSum += this._rating[i];
+        }
+        return (ratingsSum / (this._rating.length));
+    }
+
+    toggleCheckOutStatus() {
+        if (this._isCheckedOut === true) {
+            this._isCheckedOut = false
+        } else if (this._isCheckedOut === false) {
+            this._isCheckedOut = true
+        }
+    }
+
+    addRating(score) {
+        this._rating.push(score);
     }
 }
 
-class Surgeon {
-    constructor(name, department) {
-        this.name = name;
-        this.department = department;
+class Book extends Media {
+    constructor(author, title, pages, ratings, isCheckedOut) {
+        super(title, ratings, isCheckedOut);
+        this._author = author;
+        this._pages = pages;
+    }
+
+    get author() {
+        return this._author;
+    }
+    get pages() {
+        return this._pages;
     }
 }
 
-const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const poops = new Book('Me', 'poops the book', 365, [5], true);
 
-const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
-
-class Surgeon {
-    constructor(name, department) {
-        this._name = name;
-        this._department = department;
-        this._remainingVacationDays = 20;
-    }
-    get name() {
-        return this._name;
-    }
-
-    get department() {
-        return this._department;
-    }
-
-    get remainingVacationDays() {
-        return this._remainingVacationDays;
-    }
-
-    takeVacationDays(daysOff) {
-        this._remainingVacationDays = this.remainingVacationDays - daysOff;
-    }
-
-}
-
-const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
-const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
-
-class Surgeon {
-    constructor(name, department) {
-        this._name = name;
-        this._department = department;
-        this._remainingVacationDays = 20;
-    }
-    get name() {
-        return this._name;
-    }
-
-    get department() {
-        return this._department;
-    }
-
-    get remainingVacationDays() {
-        return this._remainingVacationDays;
-    }
-
-    takeVacationDays(daysOff) {
-        this._remainingVacationDays = this.remainingVacationDays - daysOff;
-    }
-
-}
-
-const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
-const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
-
-console.log(surgeonCurry._name);
-surgeonCurry.takeVacationDays(3);
-console.log(surgeonCurry._remainingVacationDays);
-
-class HospitalEmployee {
-    constructor(name) {
-        this._name = name;
-        this._remainingVacationDays = 20;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get remainingVacationDays() {
-        return this._remainingVacationDays;
-    }
-
-    takeVacationDays(daysOff) {
-        this._remainingVacationDays = this._remainingVacationDays - daysOff;
-    }
-}
-
-class HospitalEmployee {
-    constructor(name) {
-        this._name = name;
-        this._remainingVacationDays = 20;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get remainingVacationDays() {
-        return this._remainingVacationDays;
-    }
-
-    takeVacationDays(daysOff) {
-        this._remainingVacationDays -= daysOff;
-    }
-}
-
-class Nurse extends HospitalEmployee {
-    constructor(name, certifications) {
-        super(name);
-        this._certifications = certifications;
-    }
-}
-
-const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
-
-class HospitalEmployee {
-    constructor(name) {
-        this._name = name;
-        this._remainingVacationDays = 20;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get remainingVacationDays() {
-        return this._remainingVacationDays;
-    }
-
-    takeVacationDays(daysOff) {
-        this._remainingVacationDays -= daysOff;
-    }
-}
-
-class Nurse extends HospitalEmployee {
-    constructor(name, certifications) {
-        super(name);
-        this._certifications = certifications;
-    }
-}
-
-const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
-
-nurseOlynyk.takeVacationDays(5);
-console.log(nurseOlynyk.remainingVacationDays);
-
-class HospitalEmployee {
-    constructor(name) {
-        this._name = name;
-        this._remainingVacationDays = 20;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get remainingVacationDays() {
-        return this._remainingVacationDays;
-    }
-
-    takeVacationDays(daysOff) {
-        this._remainingVacationDays -= daysOff;
-    }
-}
-
-class Nurse extends HospitalEmployee {
-    constructor(name, certifications) {
-        super(name);
-        this._certifications = certifications;
-    }
-
-    get certifications() {
-        return this._certifications;
-    }
-
-    addCertification(newCertification) {
-        this._certifications.push(newCertification);
-    }
-}
-
-const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
-nurseOlynyk.takeVacationDays(5);
-console.log(nurseOlynyk.remainingVacationDays);
-nurseOlynyk.addCertification('Genetics');
-console.log(nurseOlynyk.certifications);
-
-class HospitalEmployee {
-    constructor(name) {
-        this._name = name;
-        this._remainingVacationDays = 20;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get remainingVacationDays() {
-        return this._remainingVacationDays;
-    }
-
-    takeVacationDays(daysOff) {
-        this._remainingVacationDays -= daysOff;
-    }
-    static generatePassword() {
-        return Math.floor((Math.random() * 10000) + 1);
-    }
-}
-
-class Nurse extends HospitalEmployee {
-    constructor(name, certifications) {
-        super(name);
-        this._certifications = certifications;
-    }
-
-    get certifications() {
-        return this._certifications;
-    }
-
-    addCertification(newCertification) {
-        this.certifications.push(newCertification);
-    }
-}
-
-const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
-nurseOlynyk.takeVacationDays(5);
-console.log(nurseOlynyk.remainingVacationDays);
-nurseOlynyk.addCertification('Genetics');
-console.log(nurseOlynyk.certifications);
-
+console.log(poops.getAverageRating());
