4bd3fc99ff5114a9caf1eb9faf45a4acb0ff68ba
j-hernandezinzunza <itsjosehernandez@gmail.com>
2018-08-26 19:31:25 -0600

    JavaScript modules 143-152

--- app.js
+++ app.js
@@ -1,77 +1,14 @@
-let newYearsResolutions = ['eat', 'eat more', 'eat the most'];
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length);
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('to');
+secretMessage.push('Program');
+console.log(secretMessage.length);
 
-console.log(newYearsResolutions);
-
-let newYearsResolutions = ['eat', 'eat more', 'eat the most'];
-let listItem = newYearsResolutions[0];
-console.log(listItem);
-console.log(newYearsResolutions[2]);
-console.log(newYearsResolutions[3]);
-
-console.log(newYearsResolutions);
-
-let newYearsResolutions = ['eat', 'eat more', 'eat the most'];
-let listItem = newYearsResolutions[0];
-/*
-console.log(listItem);
-console.log(newYearsResolutions[2]);
-console.log(newYearsResolutions[3]);
-console.log(newYearsResolutions);
-*/
-
-newYearsResolutions[1] = 'Learn a new language';
-
-
-let newYearsResolutions = ['eat', 'eat more', 'eat the most'];
-let listItem = newYearsResolutions[0];
-/*
-console.log(listItem);
-console.log(newYearsResolutions[2]);
-console.log(newYearsResolutions[3]);
-console.log(newYearsResolutions);
-*/
-
-newYearsResolutions[1] = 'Learn a new language';
-console.log(newYearsResolutions.length);
-
-
-let newYearsResolutions = ['eat', 'eat more', 'eat the most'];
-let listItem = newYearsResolutions[0];
-/*
-console.log(listItem);
-console.log(newYearsResolutions[2]);
-console.log(newYearsResolutions[3]);
-console.log(newYearsResolutions);
-*/
-
-newYearsResolutions[1] = 'Learn a new language';
-newYearsResolutions.push('get yoked');
-newYearsResolutions.push('marry Helena');
-console.log(newYearsResolutions);
-console.log(newYearsResolutions.length);
-
-newYearsResolutions.pop();
-console.log(newYearsResolutions);
-
-
-let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
-
-groceryList.shift();
-//console.log(groceryList);
-groceryList.unshift('popcorn');
-//console.log(groceryList);
-console.log(groceryList.slice(1, 4));
-console.log(groceryList);
-
-let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
-condiments.push('Salt');
-console.log(condiments);
-condiments = ['Salt'];
-console.log(condiments);
-const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
-utensils.pop();
-console.log(utensils);
-utensils = ['Spork'];
-
-
-
+secretMessage[7] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6, 10);
+secretMessage[6] = 'know';
+console.log(secretMessage);
