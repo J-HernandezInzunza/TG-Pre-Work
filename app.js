a3a2d8f1e0de2f628dad210145806f02a116aca5
j-hernandezinzunza <itsjosehernandez@gmail.com>
2018-08-26 19:55:57 -0600

    JavaScript modules 153-161 (For and While loops)

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
-let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
-console.log(secretMessage.length);
-secretMessage.pop();
-console.log(secretMessage.length);
-secretMessage.push('to');
-secretMessage.push('Program');
-console.log(secretMessage.length);
+let vacationSpots = ['Italy', 'Japan', 'Canada'];
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
 
-secretMessage[7] = 'right';
-secretMessage.shift();
-secretMessage.unshift('Programming');
-secretMessage.splice(6, 10);
-secretMessage[6] = 'know';
-console.log(secretMessage);
+let vacationSpots = ['Mozambique', 'Thailand', 'Bolivia'];
+
+for (let vacationSpotIndex = 0; vacationSpotIndex < vacationSpots.length; vacationSpotIndex++) {
+    console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+}
+
+
+let vacationSpots = ['Mozambique', 'Thailand', 'Bolivia'];
+
+for (let vacationSpotIndex = vacationSpots.length - 1; vacationSpotIndex >= 0; vacationSpotIndex--) {
+    console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+}
+
+console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+
+let myPlaces = ['tokyo', 'italy', 'seattle'];
+let friendPlaces = ['spain', 'tokyo', 'kansas'];
+
+for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++) {
+    //console.log(myPlaces[myPlacesIndex]);
+    for (let friendPlacesIndex = 0; friendPlacesIndex < friendPlaces.length; friendPlacesIndex++) {
+        if (myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]) {
+            console.log(myPlaces[myPlacesIndex]);
+        }
+        //console.log(friendPlaces[friendPlacesIndex]);
+    }
+}
+
+let cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+let currentCard = 'Diamond';
+
+while (currentCard != 'Spade') {
+    console.log(currentCard);
+    currentCard = cards[Math.floor(Math.random() * 4)];
+}
+console.log(currentCard); 
+
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+//console.log(groceryList);
+groceryList.unshift('popcorn');
+//console.log(groceryList);
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
